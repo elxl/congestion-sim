73024439332072d4ff8a9b5065e80ecc92cbc61b
xinling li <e1148139557@163.com>
2024-01-18 20:50:13 +0100

    first commit

---
+++ functions.py
@@ -0,0 +1,178 @@
+import random, math
+import numpy as np
+import gurobipy as gb
+from datetime import datetime, timedelta
+from structures import Passenger, Vehicle
+
+from rsome import dro, ro
+from rsome import grb_solver as grb
+
+
+def initialize_demand(network):
+    """
+    Generate passengers according to real data.
+
+    return: list of passengers, dictionary of passengers
+    """
+    demand_list = []
+    demand_id_dict = dict()
+    demand_ind = 0
+
+    random.seed(2023)
+
+    for i in range(network.demand_data.shape[0]):
+        request_time = datetime.strptime(network.demand_data.loc[i,"pu_time"], "%Y-%m-%d %H:%M:%S")
+        if (request_time >= network.start_timestamp) & (request_time < network.end_timestamp):
+            pickup_zone = network.zone_index_id_dict[int(network.demand_data.loc[i, "pu_zone"])]
+            dropoff_zone = network.zone_index_id_dict[int(network.demand_data.loc[i, "do_zone"])]
+            while True:
+                pickup_node = random.sample(network.zone_to_road_node_dict[pickup_zone], 1)[0]
+                dropoff_node = random.sample(network.zone_to_road_node_dict[dropoff_zone], 1)[0]
+                if pickup_node != dropoff_node:
+                    break
+
+            pax = Passenger(demand_ind, pickup_node, dropoff_node, request_time, None, 0.0, 0.0, None, False)
+            demand_id_dict[demand_ind] = pax
+            demand_list.append(pax)
+            demand_ind += 1
+
+    return demand_list, demand_id_dict
+
+
+def initialize_vehicle(fleet_size, network):
+    """
+    Initialize vechiles.
+
+    return: list of vehicles, dictionary of vehicles
+    """
+    vehicle_list = []
+    vehicle_id_dict = dict()
+    vehicle_ind = 0
+    init_avail_time = datetime(2019,6,network.date,network.start_time[0],0,0)
+    random.seed(2023)
+
+    zone_vehicle_number = int(math.floor(fleet_size / network.n)) # number of vehicles in each zone
+    for i in network.zone_to_road_node_dict.keys():
+        road_node_list = network.zone_to_road_node_dict[i]
+        vehicle_loc_list = random.choices(road_node_list, k=zone_vehicle_number) # sample number of vehicles locations in zone
+        for loc in vehicle_loc_list:
+            veh = Vehicle(vehicle_ind, loc, init_avail_time, loc, 0, [], [], [], [], [])
+            vehicle_id_dict[vehicle_ind] = veh
+            vehicle_list.append(veh)
+            vehicle_ind += 1
+
+    return vehicle_list, vehicle_id_dict
+
+def matching(vehicles, demands, network):
+    """
+    Match available vechiles and requests.
+
+    vehicles: list
+    demands: list
+
+    return: matched vehicle and passenger, unserved passenger
+    """
+    pickup_dist = {}
+    # All possible matching schemes
+    for veh in vehicles:
+        veh_id = veh.id
+        veh_loc = veh.current_location
+        for dem in demands:
+            dem_id = dem.id
+            dem_loc = dem.origin
+            pick_distance = network.road_distance_matrix[veh_loc, dem_loc]
+            pickup_time = network.travel_time(veh_loc, dem_loc)
+            if pickup_time <= network.maximum_waiting_time:
+                pickup_dist[(veh_id, dem_id)] = pick_distance
+
+    veh_dict = {veh.id: [] for veh in vehicles}
+    dem_dict = {dem.id: [] for dem in demands}
+
+    for i in pickup_dist:
+        veh_dict[i[0]].append(i)
+        dem_dict[i[1]].append(i)
+
+    # Matching optimization
+    m = gb.Model("matching")
+    m.setParam('OutputFlag', 0)
+    m.setParam('TimeLimit', 10)
+    # m.setParam('MIPGap', 0.01)
+
+    # Decision variables
+    x = {}
+    for i in pickup_dist:
+        x[i] = m.addVar(vtype='B', name="matching_" + str(i))
+
+    # Decision variables for unserved matching
+    y = {}
+    for dem in dem_dict:
+        y[dem] = m.addVar(vtype='B', name="unserved_" + str(dem))
+
+    # Constraints
+    m.addConstrs(gb.quicksum(x[i] for i in veh_dict[veh]) <= 1 for veh in veh_dict)
+    m.addConstrs(gb.quicksum(x[i] for i in dem_dict[dem]) + y[dem] == 1 for dem in dem_dict)
+
+    # Objectives
+    m.setObjective(network.γ * gb.quicksum(y[i] for i in dem_dict)
+                    + gb.quicksum(x[i] * pickup_dist[i] for i in pickup_dist), gb.GRB.MINIMIZE)
+    m.optimize()
+
+    # Get matched vehicle and passenger as well as the pick up distance
+    matching_list = []
+    for i, var in x.items():
+        if var.X == 1:
+            matching_list.append((i, pickup_dist[i]))
+
+    unserved_pax_list = []
+    for i, var in y.items():
+        if var.X == 1:
+            unserved_pax_list.append(i)
+
+    return matching_list, unserved_pax_list
+        
+def optimization(r, V1, O1, P, Q, n, K, a, b, d, β, γ):
+
+    model = ro.Model()
+
+    # Decision variables
+    x = model.dvar((n, n, K))
+    y = model.dvar((n, n, K))
+    O = model.dvar((n, K))
+    V = model.dvar((n, K))
+    S = model.dvar((n, K))
+    T = model.dvar((n, K))
+
+    # Positive variable constraints
+    model.st(x >= 0,y >= 0, O >= 0,S >= 0,V >=0, T>=0)
+
+    # Set constraints to force initial position of occupied and vacant vehicles
+    model.st(V[:, 0] == V1)
+    model.st(O[:, 0] == O1)
+
+    # Set constraints related to state transitions (1,2,3)
+    model.st(S == V + x.sum(axis=0) - x.sum(axis=1))
+    model.st(V[i,k+1] == S[i,k] - y[:,i,k].sum() + (Q[:,i,k] * O[:,k]).sum() for i in range(n) for k in range(K-1))
+    model.st(O[i,k+1] == y[:,i,k].sum() + (P[:,i,k] * O[:,k]).sum() for i in range(n) for k in range(K-1))
+
+    model.st(x.sum(axis=1) <= V)
+
+    # Set rebalancing constraint (4)
+    model.st(a.astype(int) * x == 0)
+
+    # Set surplus vehicle / passenger constraint (6, 7, 8, 9)
+    model.st(y.sum(axis=0) <= S)
+    model.st(y.sum(axis=0) <= r)
+    model.st(T == r - y.sum(axis=1)
+             )
+    # Set matching constraint (10)
+    model.st(b.astype(int) * y == 0)
+
+    model.min((x * d).sum()
+               + 𝛽 * (y * np.transpose(d, [1,0,2])).sum()
+               + γ * T.sum())
+    
+    model.solve(grb, display=False)
+
+    x_value = x.get()
+
+    return x_value
---
+++ structures.py
@@ -0,0 +1,228 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 27 11:01:02 2023
+
+@author: 11481
+"""
+import pandas as pd
+import numpy as np
+from collections import defaultdict
+from datetime import datetime
+
+class Passenger:
+    def __init__(self, nid, origin, destination, request_time, assign_time, wait_time, travel_time, arrival_time, leave_system):
+        self.id = nid
+        self.origin = origin
+        self.destination = destination
+        self.request_time = request_time
+        self.assign_time = assign_time
+        self.wait_time = wait_time
+        self.travel_time = travel_time
+        self.arrival_time = arrival_time
+        self.leave_system = leave_system
+        self.pickup_time = None
+
+class Vehicle:
+    def __init__(self, nid, location, available_time, current_location, status, served_passenger, rebalancing_travel_distance,
+                 pickup_travel_distance, occupied_travel_distance, rebalancing_trips):
+        """status: 0(available), 1(being rebalanced), 2(way to pick up passengers), 3(occupied) """
+        self.id = nid
+        self.location = location
+        self.available_time = available_time
+        self.current_location = current_location
+        self.status = status
+        self.served_passenger = served_passenger
+        self.rebalancing_travel_distance = rebalancing_travel_distance
+        self.pickup_travel_distance = pickup_travel_distance
+        self.occupied_travel_distance = occupied_travel_distance
+        self.rebalancing_trips = rebalancing_trips
+        self.passenger_id = None
+
+class Road:
+    """ Road segment between two intersections"""
+
+    def __init__(self, start, end, cap, t, background):
+
+        self.start = start
+        self.end = end
+        self.cap = cap
+        self.t0 = t
+        self.t = t
+        self.flow = background
+
+    def travel_time(self):
+        """ BPR """
+
+        self.t = self.t0 * (1 + 0.15*((self.flow)/self.cap)**4)
+
+        return self.t
+    
+class Network:
+    """ A complete network of New York under different congestion level"""
+
+    def __init__(self, congestion_level=1, date=27, time_start=7, time_end=9, time_interval_length=300, rebalancing_time_length=1800, matching_window=30, capacity=1800):
+
+        self.congestion_level = congestion_level
+        self.capacity = capacity
+        self.free_speed = 20
+
+        self.zone_index_id = pd.read_csv("data/NYC/zone_index_id.csv", header=None).values
+        self.zone_index_id_dict = dict()
+        for i in range(self.zone_index_id.shape[0]):
+            self.zone_index_id_dict[self.zone_index_id[i,1]] = self.zone_index_id[i,0]
+        
+        self.date=date
+        self.start_time = (time_start, 0) # Hour, Minute
+        self.end_time = (time_end, 0) # Hour, Minute
+        
+        self.time_interval_length = time_interval_length # Seconds
+        self.rebalancing_time_length = rebalancing_time_length # 6 time intervals
+        self.matching_window = matching_window # Seconds
+        data1 = pd.read_csv('data/NYC/road_network/distance.csv', header=None) / 1609.34
+        self.road_distance_matrix = data1.values
+        self.t = 3600 * self.road_distance_matrix / self.free_speed
+        data2 = pd.read_csv('data/NYC/road_network/predecessor.csv', header=None)
+        self.predecessor = data2.values
+        
+        self.start_bin = self.start_time[0] * 12
+        self.end_bin = self.end_time[0] * 12 - 1
+        self.start_timestamp = datetime(2019,6,date,self.start_time[0],0,0)
+        self.end_timestamp = datetime(2019,6,date,self.end_time[0],0,0)
+        
+        # Demand
+        data = pd.read_csv("data/NYC/processed_data/normalized_data.csv")
+        data = data[(data['bin'] >= self.start_bin) & (data['bin'] <= self.end_bin)]
+        prev_data = data[(data['month'] !=6) | (data['day'] < date)]
+        June_date_data = data[(data['month'] ==6) & (data['day'] == date)]
+        
+        gd = prev_data.groupby(['day','month'])
+        self.n = len(data['zone'].unique())
+        K = len(data['bin'].unique())
+        m = len(gd)
+        data_points = np.zeros((self.n, K, m))
+        index = 0
+        for _, df in gd:
+            y_i = df.loc[:,['zone','bin','demand']].pivot(index='zone',columns='bin',values='demand').values
+            data_points[:,:,index] = y_i
+            index += 1   
+
+        self.data_points = data_points
+            
+        self.road_node_to_zone = pd.read_csv('data/NYC/road_node_to_zone.csv', header=None).values
+        self.road_nodes = list(self.road_node_to_zone[:,0])
+        self.zone_to_road_node_dict = defaultdict(list)
+        self.road_node_to_zone_dict = dict()
+        for i in range(self.road_node_to_zone.shape[0]):
+            road_node_id = self.road_node_to_zone[i, 0]
+            zone_id = self.road_node_to_zone[i, 1]
+            self.road_node_to_zone_dict[road_node_id] = zone_id
+            self.zone_to_road_node_dict[zone_id].append(road_node_id)
+            
+        self.zone_centriod_node = pd.read_csv("data/NYC/centroid_ind_node.csv", header=None).values
+        self.centroid_to_node_dict = dict()
+        for i in range(self.zone_centriod_node.shape[0]):
+            self.centroid_to_node_dict[self.zone_centriod_node[i,0]] = self.zone_centriod_node[i,1]
+
+        # Demand information used for solving optimization problems
+        self.true_demand = June_date_data.loc[:, ['zone','bin','demand']].pivot(index='zone',columns='bin',values='demand').values
+            
+        # Demand information used for simulation
+        self.demand_data = pd.read_csv("data/NYC/demand/fhv_records_06272019.csv")
+        
+        # Problem Parameters
+        self.β = 1
+        self.γ = 1e2
+        self.average_speed = 20
+        self.maximum_waiting_time = 300    # seconds
+        self.maximum_rebalancing_time = self.time_interval_length
+
+        d = np.load("data/NYC/distance_matrix.npy") # Zone centroid distances in miles
+        self.d = np.repeat(d[:, :, np.newaxis], K, axis=2) # Repeat d to create a n x n x K matrix
+        # Hourly travel time
+        w_hourly = np.load("data/NYC/hourly_tt.npy")
+        # Hourly travel time to 288 time intervals
+        a = np.repeat(w_hourly[:,:,0][:, :, np.newaxis], 12, axis=2)
+        for i in range(1,24):
+            b = np.repeat(w_hourly[:,:,i][:, :, np.newaxis], 12, axis=2)
+            a = np.concatenate((a, b), axis=2)
+        
+        w = a * 3600
+        w = w[:,:,self.start_bin:self.end_bin+1] # travel time matrix
+        
+        self.a = (w > self.maximum_rebalancing_time)
+        self.b = (w > self.maximum_waiting_time)
+        
+        P = np.load("data/NYC/p_matrix_occupied.npy")
+        Q = np.load("data/NYC/q_matrix_occupied.npy")
+        self.P = np.repeat(P[:,:,np.newaxis], K, axis=2)
+        self.Q = np.repeat(Q[:,:,np.newaxis], K, axis=2)
+
+        self._generate_road_segment()
+            
+    def _generate_road_segment(self):
+
+        self.roads = defaultdict(lambda: None)
+
+        for i in self.road_nodes:
+            for j in self.road_nodes:
+                pre = self.predecessor[i,j]
+
+                if not np.isnan(pre):
+                    pre = int(pre)
+                    self.roads[(pre,j)] = Road(pre, j, self.capacity, self.t[pre,j], self.congestion_level*900)
+
+        # Path between OD. Generated on the fly to save time.
+        self.paths = defaultdict(lambda: None)
+
+    def get_path(self, ori, des):
+
+        if ori == des:
+            return []
+
+        if self.paths[(ori,des)] is not None:
+            return self.paths[(ori,des)]
+        else:
+            trip_path = []
+            temp_node = des
+            # Path of vehicle
+            while True:
+                pred = int(self.predecessor[ori,temp_node])
+                trip_path.insert(0, pred)
+                if pred == ori:
+                    break
+
+                temp_node = pred
+            trip_path.append(des)
+            self.paths[(ori,des)] = trip_path
+            return trip_path
+
+    def travel_time(self, ori, des):
+        """ Calculate travel time on edge (ori,des)"""
+
+        if ori == des:
+            return 0
+
+        if self.paths[(ori,des)] is not None:
+            trip_path = self.paths[ori,des]
+        else:
+            trip_path = []
+            temp_node = des
+            # Path of vehicle
+            while True:
+                pred = int(self.predecessor[ori,temp_node])
+                trip_path.insert(0, pred)
+                if pred == ori:
+                    break
+
+                temp_node = pred
+            trip_path.append(des)
+            self.paths[(ori,des)] = trip_path
+
+        travel_time = 0
+        for i in range(len(trip_path) - 1):
+            start_node = trip_path[i]
+            end_node = trip_path[i+1]
+            segment_time = self.roads[(start_node,end_node)].travel_time()
+            travel_time += segment_time
+
+        return travel_time       
